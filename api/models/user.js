7b975823dc5cf8d7b8ab73c32ecf1862d1f9d591
Jovanny Cruz <jovannypcg@yahoo.com>
2017-03-07 14:34:59 -0600

    chore(app): Remove unnecessary files

--- api/models/user.js
+++ api/models/user.js
@@ -12,10 +12,7 @@ let UserSchema = new Schema({
     first_name  : String,
     last_name   : String,
     email       : { type: String, trim: true, lowercase: true },
-    phone_number: { type: String, trim: true },
-    picture     : String,
-    password    : String,
-    moves       : [{ type: Schema.Types.ObjectId, ref: 'Move' }]
+    password    : String
 }, {
     collection: 'user',
     timestamps: {
